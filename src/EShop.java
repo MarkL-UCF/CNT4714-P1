73dcb78994a332eaa654c3db887f750fee62d86e
Mark <ma999600@ucf.edu>
2025-01-16 10:05:02 -0500

    Started using IDEA's UI creator

--- src/EShop.java
+++ src/EShop.java
@@ -21,6 +21,10 @@ public class EShop implements ActionListener{
     //instance variables
     //window for GUI
     private JFrame window = new JFrame("Nile.com");
+    private JPanel Base;
+    private JPanel Entry;
+    private JPanel Cart;
+    private JPanel Controls;
 
 
     //Windchill():  constructor
